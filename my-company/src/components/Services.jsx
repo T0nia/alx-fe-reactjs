e994c2ce60912440d776fee8a3d60f232b53edec
T0nia <edtoniaelohor@gmail.com>
2024-08-18 01:10:40 +0100

    added the four files

---
+++ my-company/src/components/Services.jsx
@@ -0,0 +1,15 @@
+function Services() {
+    return (
+      <div style={{ padding: '20px' }}>
+        <h1 style={{color: 'green', textAlign: 'center',
+        }}> Our Services</h1>
+        <ul>
+          <li style={{padding: '.4em'}}>Technology Consulting</li>
+          <li style={{padding: '.4em'}}>Market Analysis</li>
+          <li style={{padding: '.4em'}}>Product Development</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Services;

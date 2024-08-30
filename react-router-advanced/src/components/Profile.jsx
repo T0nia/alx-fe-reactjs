d3a74af35de540dd6d83c97a311fc714525af8ac
T0nia <edtoniaelohor@gmail.com>
2024-08-30 17:47:22 +0100

    modified Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
 function Profile() {
   return (
@@ -11,6 +13,10 @@ function Profile() {
           <li><Link to="settings">Profile Settings</Link></li>
         </ul>
       </nav>
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
       <Outlet />
     </div>
   );

4fd86f375fce0a6fed939defb8a19e76867b9e12
T0nia <edtoniaelohor@gmail.com>
2024-08-30 17:04:52 +0100

    modified PostsComponent

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -11,8 +11,28 @@ const fetchPosts = async () => {
 };
 
 const PostsComponent = () => {
-  // Use React Query's useQuery hook
-  const { data, error, isLoading, isError, refetch } = useQuery('posts', fetchPosts);
+  // Use React Query's useQuery hook with additional options
+  const {
+    data,
+    error,
+    isLoading,
+    isError,
+    refetch,
+    isFetching,
+    isPreviousData
+  } = useQuery('posts', fetchPosts, {
+    // Cache time (time in milliseconds to keep data in cache)
+    cacheTime: 1000 * 60 * 5, // 5 minutes
+
+    // Stale time (time in milliseconds to consider data fresh)
+    staleTime: 1000 * 60, // 1 minute
+
+    // Refetch on window focus (default is true)
+    refetchOnWindowFocus: true,
+
+    // Keep previous data while new data is being fetched
+    keepPreviousData: true
+  });
 
   // Handle loading state
   if (isLoading) return <div>Loading...</div>;
@@ -23,7 +43,9 @@ const PostsComponent = () => {
   // Render posts
   return (
     <div>
-      <button onClick={() => refetch()}>Refetch Posts</button>
+      <button onClick={() => refetch()} disabled={isFetching}>
+        {isFetching ? 'Refetching...' : 'Refetch Posts'}
+      </button>
       <ul>
         {data.map(post => (
           <li key={post.id}>
@@ -37,3 +59,4 @@ const PostsComponent = () => {
 };
 
 export default PostsComponent;
+

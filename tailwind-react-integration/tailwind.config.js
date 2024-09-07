8994f76408899609bda68061939cffb43e6c26bf
T0nia <edtoniaelohor@gmail.com>
2024-09-07 18:01:46 +0100

    made changes to tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,12 +1,12 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+  purge: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html"
   ],
+  darkMode: 'media', // or 'class' if you want to enable dark mode manually
   theme: {
     extend: {},
   },
-  plugins: [],
+  variants: [],
 }

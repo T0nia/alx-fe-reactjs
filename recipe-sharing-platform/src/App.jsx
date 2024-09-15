60a2d5a23963664f5bd4156a14f515edf6379636
T0nia <edtoniaelohor@gmail.com>
2024-09-15 13:26:36 +0100

    added homepage

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,3 +1,14 @@
+import React from 'react';
+import HomePage from './components/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+//
+}
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'

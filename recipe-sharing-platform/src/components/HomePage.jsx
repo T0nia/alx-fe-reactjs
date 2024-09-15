6906b993f3add28381699b54d201717e225316a7
T0nia <edtoniaelohor@gmail.com>
2024-09-15 15:01:20 +0100

    modified homepage

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -15,8 +15,10 @@ const HomePage = () => {
                  <div key={recipe.id} className="bg-green-100 w-64 md:w-60 flex flex-col rounded shadow hover:scale-110 transition-transform duration-500 ease-in-out">
                     <Link to={`/recipe/${recipe.id}`}>
                         <img src={recipe.image} alt="food" className="rounded h-52 w-full object-cover" />
+                        
                         <h1 className="text-green-800 text-base font-bold px-3 mt-6 uppercase hover:text-green-700">{recipe.title}</h1>
                         <p className="max-w-72 mt-2 mb-7 px-3 text-gray-600 text-sm">{recipe.summary}</p>
+                        <p className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4"></p>
                     </Link>
                 </div>
             ))}

accec089c8405676269e43d7e07d91393e510597
T0nia <edtoniaelohor@gmail.com>
2024-09-22 11:37:30 +0100

    set up react for github-user-search

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <div className='container mx-auto'>
+      <h1>GitHub User Search</h1>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
+
